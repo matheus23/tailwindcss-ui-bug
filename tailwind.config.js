38437fa166ab618f5e24b6ae1e7e0259a9dafb4d
Philipp Krüger <philipp.krueger1@gmail.com>
2021-02-03 19:18:08 +0100

    Initial Commit.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: {},
+    variants: [], // We can do variants in elm code
+    plugins: [require("@tailwindcss/ui")],
+    future: {
+        removeDeprecatedGapUtilities: true,
+    },
+};
